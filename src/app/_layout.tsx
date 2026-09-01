9130fc4c68bc5a09d9dd919397b2126b285b863c
Edney Lazaro <edneylazaro@hotmail.com>
2026-09-01 18:40:34 +0100

    fix starting page

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -18,7 +18,7 @@ function NavigateGate() {
     const inAuthGroup = segments[0] === "(auth)";
 
     if (!isAuthenticated && !inAuthGroup) {
-      router.replace(Paths.home);
+      router.replace(Paths.login);
     } else if (isAuthenticated && inAuthGroup) {
       router.replace(Paths.home);
     }
